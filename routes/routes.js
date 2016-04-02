ffccae418fd96b7077cb330bf25a73a42569c8be
Ben Stuijts Node.js <stuijts@spijkenisse.nl>
2016-04-02 21:59:52 +0000

    all articles review updated

--- routes/routes.js
+++ routes/routes.js
@@ -15,7 +15,7 @@ router.use(function (req, res, next) {
         pathname: req.originalUrl
     });    
     
-    Article._read({}, {title: 1, slug: 1, image: 1, _id: 0})
+    Article._read({}, {title: 1, slug: 1, image: 1, backgroundColor: 1, _id: 0})
         .then(function(result){
             res.locals = {
                 author: "Ben Stuijts",
